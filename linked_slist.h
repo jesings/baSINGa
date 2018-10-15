5857fdedf34e4d846cd98f45d9861824cf3b453e
jesings <jsinger@stuy.edu>
2018-10-15 10:37:07 -0400

    headed

---
+++ linked_slist.h
@@ -0,0 +1,3 @@
+struct snode* make_snode(char* song_name, char* artist_name);
+struct snode* add_front(struct snode* head, struct snode* to_insert);
+struct snode* add_alph(struct snode* head, struct snode* to_insert);
