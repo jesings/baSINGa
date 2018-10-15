0a773e268ff3ae7c45465bb04b051ec11e7594da
jesings <jsinger@stuy.edu>
2018-10-15 14:28:47 -0400

    finished adding (but not testing) slist functions

--- linked_slist.c
+++ linked_slist.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "snode.h"
-
 struct snode* make_snode(char* song_name, char* artist_name){
     struct snode* new_snode = malloc(sizeof(struct snode));
     strcpy(new_snode->name,song_name);
@@ -46,4 +45,50 @@ struct snode* add_alph(struct snode* head, struct snode* to_insert){
         break;
     }
     return head;
+}
+void print_lib(struct snode* head){
+    printf("song: %s, artist: %s \n",head->name,head->artist);
+    if(head->next)
+        print_lib(head->next);
+}
+struct snode* find_song_an(char* artist, char* songname, struct snode* node){
+    if(!node) return NULL;
+    if(!strcmp(node->artist,artist)&&!strcmp(node->name,songname))
+        return node;
+    return find_song_an(artist,songname,node->next);
+}
+struct snode* find_song_a(char* artist, struct snode* node){
+    if(!node) return NULL;
+    if(!strcmp(node->artist,artist))
+        return node;
+    return find_song_a(artist,node->next);
+}
+int len(struct snode* head){
+    if(head) return 1 + len(head->next);
+    return 0;
+}
+struct snode* rand_song(struct snode* head){
+    int i = rand()%len(head);
+    for(;i>0;i--){
+        head = head->next;
+    }
+    return head;
+}
+void remove_snode(struct snode* to_remove, struct snode* head){
+    struct snode* prev;
+    while(head){
+        if(head==to_remove){
+            if(prev) prev->next = head;
+            free(to_remove);
+            break;
+        }
+        prev = head;
+        head = head->next;
+    }
+    printf("element not found in list\n");
+}
+void remove_slist(struct snode* head){
+    if(head->next)
+        remove_slist(head->next);
+    free(head);
 }
--- linked_slist.h
+++ linked_slist.h
@@ -1,3 +1,10 @@
 struct snode* make_snode(char* song_name, char* artist_name);
 struct snode* add_front(struct snode* head, struct snode* to_insert);
 struct snode* add_alph(struct snode* head, struct snode* to_insert);
+struct snode* find_song_an(char* artist, char* songname, struct snode* node);
+struct snode* find_song_a(char* artist, struct snode* node);
+void print_lib(struct snode* head);
+struct snode* rand_song(struct snode* head);
+int len(struct snode* head);
+void remove_snode(struct snode* to_remove, struct snode* head);
+void remove_slist(struct snode* head);
