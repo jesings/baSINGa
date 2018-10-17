900de8e6e00fdeb8b41262a18b62ad040e0170e6
jesings <jsinger@stuy.edu>
2018-10-17 10:22:18 -0400

    slight edits

--- sbucket.c
+++ sbucket.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 #include "snode.h"
-#include "sbucket.h"
+#include "linked_slist.h"
 
 struct snode * add_sbucket(struct snode * snew, struct snode * sbucket) {
   if ('a' <= snew->artist  && snew->artist <= 'z') {
@@ -14,8 +15,18 @@ struct snode * add_sbucket(struct snode * snew, struct snode * sbucket) {
   return sbucket;
 }
 
-struct snode * find_s(char * sartist, char * sname) {
-  
+struct snode * find_s(struct snode* sbucket_arr,char * sartist, char * sname) {
+  return find_song_an(sartist,sname,sbucket_arr[tolower(sartist[0])-'a']);
 }
 
+void print_single_letter(struct snode* sbucket_arr,char letter){
+    print_slist(sbucket_arr[tolower(letter)-'a']);
+}
+void print_single_artist(struct snode* sbucket_arr,char* artist_name){
+    
+}
 
+void print_whole_lib(struct snode* sbucket_arr){
+    for(int i = 0;i<27;i++)
+        print_slist(sbucket_arr[i]);
+}
